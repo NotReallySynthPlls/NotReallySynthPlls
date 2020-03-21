a737c246b25bb905acda23c2727100bba51ba5e7
Dan Fritchman <dan@fritch.mn>
2020-03-20 21:32:14 -0700

    Phase Locking, Clean-Up

--- Canary/src/pll.sv
+++ Canary/src/pll.sv
@@ -1,85 +1,78 @@
 `timescale 1fs/1fs
-
+`define NUM_STAGES 15
 
 module pll (
-    input wire refclk,
-    output reg pclk
+    input  wire refclk,
+    input  wire resetn,
+    input  wire brake,
+    output reg  pclk
 );
-    // From bottom, DCO gain
+    parameter N = 32;
+    logic ready;
     real err_real, freq, period, stage_delay;
+    int accum, accum_m1, targ_phase, err;
+    int dco_int_count, dco_frac_state, dco_phase, dco_phase_m1, dco_phase_diff;
 
-    reg [31:0] accum_m1;
-    wire [31:0] accum;
-
-    // Ring-State DCO
-    parameter STAGE_DELAY = 10*1000;
-    // Numerical (30-state) DCO
-    int dco_frac_state, dco_frac_state_samp, dco_frac_state_samp_m1;
-
-    always #(stage_delay*1e15) begin // #STAGE_DELAY begin // 
-        if (dco_frac_state >= 'd29) dco_frac_state = 'd0;
+    // DCO
+    always #(stage_delay*1e15) begin 
+        if (!resetn) dco_frac_state = `NUM_STAGES; // "Half way" through cycle
+        else if (dco_frac_state >= 2 * `NUM_STAGES - 1) dco_frac_state = 0;
         else dco_frac_state = dco_frac_state + 1;
 
-        if (dco_frac_state >= 'd15) pclk = 'b0;
-        else pclk = 'b1;
+        if (dco_frac_state >= `NUM_STAGES) pclk = 1'b0;
+        else pclk = 1'b1;
     end 
     // More realistic DCO would include stuff like this,
     // and accompanying decode logic 
     // reg [14:0] dco_frac_state, dco_frac_state_samp, dco_frac_state_samp_m1;
-
     // initial dco_frac_state = 15'b010101010101010;
     // always @ (dco_frac_state) #STAGE_DELAY dco_frac_state <= {~dco_frac_state[13:0], ~dco_frac_state[14]};
-    // // Primary Output
     // assign pclk = dco_frac_state[0];
 
-    // Phase Sampling 
-    int dco_int_count, dco_int_count_samp, dco_int_count_samp_m1;
-    int dco_phase, dco_phase_m1, dco_phase_diff;
-    // Reference-Phase Generation
-    parameter N = 4;
-    int targ_phase, err;
-    initial begin 
+    // Integer DCO Cycle Count
+    always @ (posedge pclk or negedge resetn) begin 
+        if (!resetn) dco_int_count = 0;
+        else dco_int_count = dco_int_count + 1;
+    end 
+    // Main Sampling-Time Action
+    always @ (posedge refclk or negedge resetn) begin
+        if (!resetn) begin 
+            pclk = 1'b0; // DCO reset, as via a hard loop-break 
+            dco_frac_state = `NUM_STAGES; 
 
-        pclk = 'b0;
-        dco_frac_state = 'd15;
+            // Reset all our sampled signals
+            dco_int_count = 0;
+            dco_phase = 0;
+            dco_phase_m1 = 0;
+            dco_phase_diff = 0;
+            targ_phase = 'd0;
+            accum_m1 = 0;
+            ready = 1'b0;
+        end else begin
+            // Sample DCO Phase
+            dco_phase_m1 = dco_phase;  // Keep one past sample, in case we want frequency feedback 
+            dco_phase = 2 * `NUM_STAGES * dco_int_count + dco_frac_state;
+            
+            if (ready) begin // Make FB Loop updates. Several require a cycle of initialization. 
+                targ_phase = targ_phase + 2 * `NUM_STAGES * N;
+                accum_m1 = accum;
+                err = targ_phase - dco_phase; // Note FB Loop inverts here!
 
-        dco_int_count = 0;
-        dco_phase = 0;
-        dco_phase_m1 = 0;
-        dco_phase_diff = 0;
-
-        targ_phase = 'd0;
-
-        accum_m1 = 0;
-
-        
+                // Frequency measurement 1-z**-1
+                // dco_phase_diff = dco_phase - dco_phase_m1; 
+                // err = 2 * `NUM_STAGES * N - dco_phase_diff; // Note FB Loop inverts here!
+            end else begin 
+                targ_phase = dco_phase;
+                ready = 1'b1;
+            end
+        end 
     end
-    always @ (posedge pclk) dco_int_count = dco_int_count + 1;
-    always @ (posedge refclk) begin
-        accum_m1 <= accum;
-
-        dco_frac_state_samp_m1 = dco_frac_state_samp;
-        dco_frac_state_samp = dco_frac_state;
-
-        dco_int_count_samp_m1 = dco_int_count_samp;
-        dco_int_count_samp = dco_int_count;
-
-        dco_phase_m1 = dco_phase;
-        dco_phase = 30*dco_int_count + dco_frac_state;
-        dco_phase_diff = dco_phase - dco_phase_m1; // FLL-ize
-
-        targ_phase = targ_phase + 30*N;
-        // err = targ_phase - dco_phase; // FB Loop inverts here!
-        err = 30*N - dco_phase_diff; // FLL-ize
-    end
-
-    
 
     // Loop Filter
     assign accum = err + accum_m1;
 
-    parameter KP = 500;
-    parameter KI = 100;
+    parameter KP = 400;
+    parameter KI = 60;
 
     int di, dp, dctrl;
     assign dp = err * KP;
@@ -87,49 +80,42 @@ module pll (
     assign dctrl = di + dp;
 
     // DCO Gain
-    parameter real F0 = 4.1e9;
+    parameter real F0 = 3e9;
     parameter real KDCO = 10000.0; // Hz/code
     
     assign err_real = 1.0 * err;
     assign freq = F0 + dctrl * KDCO;
     assign period = 1 / freq;
-    assign stage_delay = period / 30;
+    assign stage_delay = period / 2 / `NUM_STAGES;
 
 endmodule
 
 module tb ();
-
-    reg refclk;
-    parameter REFCLK_PERIOD = 1000*1000;
-
-    initial begin
+    logic refclk, pclk, resetn, brake;
+    parameter REFCLK_PERIOD = 8*1000*1000;
+    
+    initial begin  // TB Setup
         $dumpfile("test.vcd");
         $dumpvars;
 
-        // #(500*1000*1000);
-        #(100000*REFCLK_PERIOD);
+        resetn = 1'b0;
+        brake = 1'b0;
+        #(1.5*REFCLK_PERIOD);
+        resetn = 1'b1;
+    end
+    
+    initial begin  // Ref-Clock Generation
+        // Sadly these delays (in fs) get too big for ints!
+        // vcs has a sad tendency to replace them with zero. 
+        refclk = 1'b0;
+        for (int i=200; i>0; i--) begin
+            #(REFCLK_PERIOD/2);
+            refclk = !refclk;
+        end
         $finish;
     end
 
-    // Ref Clock Gen
-    initial refclk = 1'b0;
-    always  #(REFCLK_PERIOD/2) refclk = !refclk;
-
     // DUT 
-    wire pclk;
-    pll i_pll(refclk, pclk);
-
-    // Divider 
-    reg [31:0] div_count;
-    reg divclk;
-    initial divclk = 1'b0;
-    initial div_count = 'd0;
-    always @(posedge pclk) begin 
-        if (div_count >= 2) begin 
-            divclk = !divclk;
-            div_count = 'd0;
-        end else div_count = div_count + 1;
-    end
+    pll i_pll(refclk, resetn, brake, pclk);
 
 endmodule
-
