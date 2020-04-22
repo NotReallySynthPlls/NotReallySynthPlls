fa2f4c1c70f2777c8f628c8b005ebbe3f5aab8cf
Dan Fritchman <dan@fritch.mn>
2020-04-21 17:40:13 -0700

    Add freq loop, TDC-based phase loop. Combining them TBC

--- Canary/src/pll.sv
+++ Canary/src/pll.sv
@@ -10,9 +10,11 @@ module dco_and_phase_sampling (
     output logic pclk, 
     output int dco_phase
 );
-    parameter real F0 = 3e9;
+    parameter real F0   = 3.99e9;
+    parameter real FMIN = 3e9;
+    parameter real FMAX = 5e9;
     parameter real KDCO = 5000.0; // Hz/code
-    real freq, period, stage_delay;
+    real freq, freq_calc, period, stage_delay;
     int dco_int_count, dco_frac_state;
 
     // DCO
@@ -46,11 +48,12 @@ module dco_and_phase_sampling (
             dco_phase = 0;
 
         end else begin  // Sample DCO Phase
-            dco_phase = 2 * `NUM_STAGES * dco_int_count + dco_frac_state;
+            dco_phase <= 2 * `NUM_STAGES * dco_int_count + dco_frac_state;
         end 
     end
     
-    assign freq = F0 + dctrl * KDCO;
+    assign freq_calc = F0 + dctrl * KDCO;
+    assign freq = (freq_calc < FMIN ? FMIN : (freq_calc > FMAX ? FMAX : freq_calc) ); // Limiting
     assign period = 1 / freq;
     assign stage_delay = period / 2 / `NUM_STAGES;
 
@@ -66,7 +69,7 @@ module dco_and_phase_sampling_ctrl (
 );
     // Control-only phase-accumulator model 
     
-    parameter real F0 = 3e9;
+    parameter real F0 = 4e9;
     parameter real KDCO = 5000.0; // Hz/code
     real freq, period;
 
@@ -91,15 +94,18 @@ module pll (
     input  wire refclk,
     input  wire resetn,
     input  wire brake,
+    input  int  divn,
     output reg  pclk
 );
 
     // DCO Gain
-    parameter real F0 = 3e9;
+    parameter real F0 = 4e9;
     parameter real KDCO = 5000.0; // Hz/code
 
-    parameter int KP = 400;
-    parameter int KI = 40;
+    parameter int KP = 10;
+    parameter int KI = 1;
+    parameter int KP_FREQ = 0;
+    parameter int KI_FREQ = 400;
 
     int di, dp, dctrl;
 
@@ -108,7 +114,6 @@ module pll (
     parameter int BRAKE_CODE = BRAKE_DELTA_F / KDCO / KI;
     parameter int BRAKE_DIV = 10 * 2 * `NUM_STAGES;
 
-    parameter N = 32;
     logic ready;
     real err_real;
     int accum, accum_m1, targ_phase, err;
@@ -124,10 +129,9 @@ module pll (
         ready = 0;
     end
 
-    assign freq_target = 2 * `NUM_STAGES * N - brake_div_delta;
+    assign freq_target = 2 * `NUM_STAGES * divn - brake_div_delta;
 
-    // CONTROLLER ONLY EDITION!
-    dco_and_phase_sampling_ctrl i_dco (
+    dco_and_phase_sampling i_dco (
         .dctrl(dctrl),
         .refclk(refclk),
         .resetn(resetn),
@@ -170,7 +174,7 @@ module pll (
 
                 // Frequency measurement 1-z**-1
                 // dco_phase_diff = dco_phase - dco_phase_m1; 
-                // err <= 2 * `NUM_STAGES * N - dco_phase_diff; // Note FB Loop inverts here!
+                // err <= 2 * `NUM_STAGES * divn - dco_phase_diff; // Note FB Loop inverts here!
             end 
             else begin 
                 targ_phase <= dco_phase + freq_target;
@@ -179,16 +183,69 @@ module pll (
         end 
     end
 
+    // Frequency Count
+    int cycle_count, cycle_count_m1, fmeas, fmeas_startup, freq_err;
+    logic fmeas_ready;
+    always @(posedge pclk or negedge resetn) begin
+        if (!resetn) begin 
+            cycle_count = 0;
+            cycle_count_m1 = 0;
+        end else begin 
+            cycle_count = cycle_count + 1;
+        end
+    end 
+    always @(posedge refclk or negedge resetn) begin 
+        if (!resetn) begin 
+            fmeas = 0;
+            fmeas_ready = 1'b0;
+            fmeas_startup = 2;
+        end else begin 
+            fmeas = cycle_count - cycle_count_m1;
+            cycle_count_m1 = cycle_count;
+            freq_err = divn - fmeas;
+            if (fmeas_startup == 0) fmeas_ready = 1'b1;
+            else fmeas_startup = fmeas_startup - 1;
+        end
+    end
+
+    // Feedback Divider
+    int div_count;
+    logic fbclk;
+    always @(posedge pclk or negedge resetn) begin
+        if (!resetn || div_count == 0) begin
+            div_count = divn-1;
+        end else begin 
+            div_count = div_count - 1;
+        end
+    end 
+    assign fbclk = div_count >= divn/2;
+
+    // TDC
+    int phase_targ, phase_meas, phase_err, tdc_out;
+    logic new_fb;
+    always @(posedge fbclk) begin 
+        phase_meas = $time;
+        new_fb = 1'b1;
+    end
+    always @(posedge refclk) begin
+        if (!new_fb) tdc_out = 4000; // Feedback late & outta range
+        else begin 
+            phase_targ = $time - 4e6; // Offset
+            phase_err = (phase_meas - phase_targ) / 1e3;
+            tdc_out = phase_err > 4000 ? 4000 : phase_err < -4000 ? -4000 : phase_err;
+        end
+        new_fb = 1'b0;
+    end
+
     // Loop Filter
-    assign err = ready ? targ_phase - dco_phase : 0; // Note FB Loop inverts here
+    assign err = fmeas_ready ? tdc_out : 0; // TDC Loop
+    // assign err = fmeas_ready ? freq_err : 0; // FLL
+    //assign err = ready ? targ_phase - dco_phase : 0; // Note FB Loop inverts here
     assign accum = err + accum_m1 - brake_code_delta;
     assign dp = err * KP;
     assign di = accum * KI;
     assign dctrl = di + dp;
     assign err_real = 1.0 * err;
-
-    
-    
 
 endmodule
 
@@ -206,14 +263,14 @@ module tb ();
         resetn = 1'b1;
 
         #(1000*1000*1000);
-        brake = 1'b1;
+        // brake = 1'b1;
     end
     
     initial begin  // Ref-Clock Generation
         // Sadly these delays (in fs) get too big for ints!
         // vcs has a sad tendency to replace them with zero. 
         refclk = 1'b0;
-        for (int i=800; i>0; i--) begin
+        for (int i=1600; i>0; i--) begin
             #(REFCLK_PERIOD/2);
             refclk = !refclk;
         end
@@ -221,6 +278,8 @@ module tb ();
     end
 
     // DUT 
-    pll i_pll(refclk, resetn, brake, pclk);
+    parameter N = 32;
+
+    pll i_pll(refclk, resetn, brake, N, pclk);
 
 endmodule
